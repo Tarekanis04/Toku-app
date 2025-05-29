b7c59d841aa21d6806335f5817de2c7de711aa89
Tarekanis04 <tarek.anis04@gmail.com>
2025-05-29 14:58:53 +0300

    Add docs for GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -90,7 +90,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "52955aaa0d0a8ff3f1a9a28f15d3350d
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "55afe676bdcc3f926b2017a7514a52bf",
+"flutter_bootstrap.js": "21e18e9691bd7b86210c6e6defa0b701",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
