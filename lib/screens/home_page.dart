5381249e3437844ecbe1144daa68b32312de4407
Tarekanis04 <tarek.anis04@gmail.com>
2025-05-29 15:06:00 +0300

    Add docs for GitHub Pages

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -22,45 +22,47 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ),
-        body: Column(
-          children: [
-            Category(
-              ontap: () {
-                Navigator.push(context, MaterialPageRoute(builder:(BuildContext context){
-                  return NumbersPage();
-                }));
-              },
-              color: Color.fromARGB(255, 49, 91, 119),
-              text: 'Numbers',
-            ),
-            Category(
-              ontap: () {
-                Navigator.push(context, MaterialPageRoute(builder:(BuildContext context){
-                  return FamilyMembers();
-                }));
-              },
-              color: Color.fromARGB(255, 126, 77, 60),
-              text: 'Fsmily Members',
-            ),
-            Category(
-              ontap: (){
-                Navigator.push(context, MaterialPageRoute(builder:(BuildContext context){
-                  return ColorsPage();
-                }));
-              },
-              color: Color.fromARGB(255, 68, 123, 27),
-              text: 'Colors',
-            ),
-            Category(
-              ontap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return PhrasesPage();
-                },));
-              },
-              color: Color.fromARGB(255, 57, 36, 94),
-              text: 'Phrases',
-            ),
-          ],
+        body: Expanded(
+          child: Column(
+            children: [
+              Category(
+                ontap: () {
+                  Navigator.push(context, MaterialPageRoute(builder:(BuildContext context){
+                    return NumbersPage();
+                  }));
+                },
+                color: Color.fromARGB(255, 49, 91, 119),
+                text: 'Numbers',
+              ),
+              Category(
+                ontap: () {
+                  Navigator.push(context, MaterialPageRoute(builder:(BuildContext context){
+                    return FamilyMembers();
+                  }));
+                },
+                color: Color.fromARGB(255, 126, 77, 60),
+                text: 'Fsmily Members',
+              ),
+              Category(
+                ontap: (){
+                  Navigator.push(context, MaterialPageRoute(builder:(BuildContext context){
+                    return ColorsPage();
+                  }));
+                },
+                color: Color.fromARGB(255, 68, 123, 27),
+                text: 'Colors',
+              ),
+              Category(
+                ontap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return PhrasesPage();
+                  },));
+                },
+                color: Color.fromARGB(255, 57, 36, 94),
+                text: 'Phrases',
+              ),
+            ],
+          ),
         ),
       );
   }
